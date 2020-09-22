325a0ed78c9411d4eacdaa0b3357b571ad9d8b01
Pritom Sarkar <pritomsarkar1997@gmail.com>
2020-09-22 20:41:36 +0530

    "react chaenges"

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// functional componemt
+function Greet(){
+    return <h1>Hello Pritom</h1>
+}
+
+// const GreetArrow = () => <h1>Hello Pritom Arrow function</h1>
+
+export default Greet;
